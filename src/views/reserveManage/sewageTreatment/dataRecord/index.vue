3478a7997d50030f0f2b84843329b9a79786eb83
zhangke163 <18435155931@163.com>
2019-06-14 16:38:30 +0800

    新增预留接口目录

---
+++ src/views/reserveManage/sewageTreatment/dataRecord/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="app-container">
+    我是数据记录
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  },
+  filters: {
+   
+  },
+  created() {
+  },
+  methods: {
+   
+  }
+}
+</script>
---
+++ src/views/reserveManage/sewageTreatment/monitorControl/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="app-container">
+    我是监测监控
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  },
+  filters: {
+   
+  },
+  created() {
+  },
+  methods: {
+   
+  }
+}
+</script>
