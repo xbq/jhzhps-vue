7a384f95eb41aa1275c26c52e92d56860cb19e4e
zhangke163 <18435155931@163.com>
2019-06-14 11:33:58 +0800

    新增监测预警目录

---
+++ src/views/detection/rainSystem/simulatorAnalyze/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="app-container">
+    我是雨水系统的淹没分析
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  },
+  filters: {
+   
+  },
+  created() {
+  },
+  methods: {
+   
+  }
+}
+</script>
